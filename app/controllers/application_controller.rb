10e57c54c00195c0e8bfc2cef03acae2d5440452
Reid <rlj0713@gmail.com>
2021-04-04 20:29:34 -0400

    commit tree 217917ae78aaed24273705584c25b792ec17ff97

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
+
+    post '/team' do
+        # binding.pry
+        erb :team
+    end
 
 end
